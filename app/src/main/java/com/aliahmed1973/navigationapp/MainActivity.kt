a76358c1c386cdbc5ffed89f295395cd6d3f6092
AliAhmed1973 <eng_aliahmed1973@yahoo.com>
2022-12-14 20:33:22 +0200

    Add BottomNavigation

--- app/src/main/java/com/aliahmed1973/navigationapp/MainActivity.kt
+++ app/src/main/java/com/aliahmed1973/navigationapp/MainActivity.kt
@@ -4,11 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.aliahmed1973.navigationapp.ui.theme.NavigationAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,14 +35,107 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun NavApp() {
     NavigationAppTheme {
-       Scaffold(bottomBar = {
-           BottomNavigation() {
+        val navController = rememberNavController()
+        val currentBackStack by navController.currentBackStackEntryAsState()
+        val currentDestination = currentBackStack?.destination
+        Scaffold(bottomBar = {
+            BottomNavigation() {
+                BottomNavigationItem(
+                    icon = { Icon(Icons.Filled.Home, null) },
+                    label = { Text(text = "Home") },
+                    selected = currentDestination?.hierarchy?.any { it.route == Screen.Home.route } == true,
+                    onClick = {
+                        navController.navigate(Screen.Home.route) {
+                            popUpTo(
+                                navController.graph.findStartDestination().id
+                            ) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    })
+                BottomNavigationItem(
+                    icon = { Icon(Icons.Filled.Favorite, null) },
+                    label = { Text(text = "Favorite") },
+                    selected = currentDestination?.hierarchy?.any { it.route == Screen.Favorite.route } == true,
+                    onClick = { navController.navigate(Screen.Favorite.route){
+                        popUpTo(
+                            navController.graph.findStartDestination().id
+                        ) {
+                            saveState = true
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    } })
+            }
+        }) {
+            NavHost(
+                navController = navController,
+                startDestination = Screen.Home.route,
+                Modifier.padding(it)
+            )
+            {
+                composable(route = Screen.Home.route)
+                {
+                    HomeScreen()
+                }
+                composable(route = Screen.Favorite.route)
+                {
+                    FavoriteScreen()
+                }
+            }
 
-               
-           }
-       }) {
-
-       }
+        }
     }
 }
+
+@Composable
+fun HomeScreen() {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Home", textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+fun FavoriteScreen() {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Favorite", textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+fun DetailsScreen() {
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Details", textAlign = TextAlign.Center
+        )
+    }
+}
+
+
+sealed class Screen(val route: String) {
+    object Home : Screen("Home")
+    object Favorite : Screen("Favorite")
+    object Details : Screen("Details")
+
+}
+
+
 
